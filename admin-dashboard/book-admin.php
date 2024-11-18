6a266247583b77c6ff08fff7f86e7848bfb9dcf8
Aeron Paul Daliva <66550026+paul24601@users.noreply.github.com>
2024-11-18 10:51:48 +0800

    update

--- admin-dashboard/book-admin.php
+++ admin-dashboard/book-admin.php
@@ -1,5 +1,9 @@
 <?php
-include $_SERVER['DOCUMENT_ROOT'] . '/admin/classes/book.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+include $_SERVER['DOCUMENT_ROOT'] . '/PLVIL/admin/classes/book.php';
 $db = new DBConnection();
 ?>
 <?php
