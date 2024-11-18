1efc0dd174653e756455765d5125f3ecb3fcf9f6
Aeron Paul Daliva <66550026+paul24601@users.noreply.github.com>
2024-11-18 10:33:09 +0800

    update

--- categories.php
+++ categories.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once $_SERVER['DOCUMENT_ROOT'] . '/PLVIL/admin/utility/DBConnection.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/admin/utility/DBConnection.php';
 
 class BookCategory {
     public $conn;
--- recent_books.php
+++ recent_books.php
@@ -5,7 +5,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 
-require_once $_SERVER['DOCUMENT_ROOT'] . '/PLVIL/admin/utility/DBConnection.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/admin/utility/DBConnection.php';
 
 class Book {
     public $conn;
