97b119ce9900de69bc990e86e4cf4740ac482e31
Aeron Paul Daliva <66550026+paul24601@users.noreply.github.com>
2024-12-09 22:40:16 +0800

    Add webicon for each user page

--- search_results.php
+++ search_results.php
@@ -87,6 +87,9 @@ $conn->close();
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    
+    <!-- For High-Resolution Icons -->
+    <link rel="icon" href="assets/plvil-logo.svg" type="image/svg+xml">
 
     <link
         href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
