97516ce1ea747e636db327816e76a2e0537a19ae
Aeron Paul Daliva <66550026+paul24601@users.noreply.github.com>
2024-12-12 22:17:30 +0800

    REvision

--- admin-dashboard/book-admin.php
+++ admin-dashboard/book-admin.php
@@ -806,9 +806,6 @@ $userName = $userType === 'student-admin' ? 'Student Admin' : 'Library Admin';
             }
         });
     </script>
-    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js"></script>
-    <!-- jQuery -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <!-- DataTables JS -->
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
